f8a238fb134d2ff659ffb6b6acbbb517ab75338d
nishshanka <nakandala@hotmail.com>
2019-04-28 13:28:11 +0530

    first commit

---
+++ Meg1/Controllers/StudentController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Meg1.Models.StudentViewModels;
+
+
+namespace Meg1.Controllers
+{
+    public class StudentController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+           
+            return View();
+        }
+        [HttpGet]
+        public IActionResult AddArticles()
+        {
+            var addarticles = new AddArticleViewModel();
+            return View(addarticles);
+        }
+        [HttpPost]
+        public string AddArticles(AddArticleViewModel addarticles)
+        {
+            AddArticleViewModel ad = new AddArticleViewModel();
+            addarticles.Name = ad.Name;
+            addarticles.Faculty = ad.Faculty;
+            addarticles.Title = ad.Title;
+            addarticles.Description = ad.Description;
+            addarticles.Document = ad.Document;
+            addarticles.Image = ad.Image;
+
+
+            return AddArticles (ad);
+            
+        }
+        
+    }
+}
---
+++ Meg1/Models/StudentViewModels/AddArticleViewModel.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using Microsoft.AspNetCore.Http;
+
+namespace Meg1.Models.StudentViewModels
+{
+    public class AddArticleViewModel 
+    {
+        public string Name { get; set; }
+
+        public int Faculty { get; set; }
+
+        [Required]
+        public string Title { get; set; }
+
+        public string Description { get; set; }
+
+        public IFormFile Document { get; set; }
+
+        public IFormFile Image { get; set; }
+
+
+
+
+
+    }
+}
